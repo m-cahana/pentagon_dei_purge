e94ccce3d42f44fc8891ab912ca71d2c8dd96699
Michael Cahana <cahanamichael@gmail.com>
2025-03-17 23:02:15 -0400

    black scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,71 @@
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     </p>
+
+    <TitleScrolly 
+    titlesList={
+      [
+        'Observance for Black History Month',
+        'Quantico celebrates Black History Month at prayer breakfast',
+        'Tuskegee Airman describes service, time as POW during virtual call with 332nd AEW',
+        'Marine Corps Observes African American / Black History Month', 
+        'African American/Black Innovations ... where would we be without them?',
+        'Gospel celebration',
+        'FACES of MCIPAC - African American U.S. Marines share their stories',
+        'African American Heritage Open Mic Night',
+        'Airmen sample soul food',
+        'Goodfellow Celebrates Juneteenth',
+        'Martin Luther King Jr. special observance',
+        '2nd Annual Soul Food Taste',
+        'Vance Marchbanks Jr.',
+        'Civil rights activist shares legacy of love, community and action amid anecdotes of working with Dr. Martin Luther King Jr.',
+        "Respecting slaves' value at Mount Vernon",
+        'Miramar pauses to acknowledge selfless service of Tuskegee Airmen',
+        'Elayne Arrington'
+      ]}
+    highlightMap={{
+      0: [
+        'Observance for Black History Month',
+        'Quantico celebrates Black History Month at prayer breakfast',
+        'Marine Corps Observes African American / Black History Month', 
+        'African American Heritage Open Mic Night',
+        'Goodfellow Celebrates Juneteenth',
+        'Martin Luther King Jr. special observance',
+      ],
+      1: [
+        'African American/Black Innovations ... where would we be without them?',
+        'FACES of MCIPAC - African American U.S. Marines share their stories',
+      ],
+      2: [
+        'Airmen sample soul food',
+        'Gospel celebration',
+        '2nd Annual Soul Food Taste',
+      ],
+      3: [
+         "Respecting slaves' value at Mount Vernon",
+         'Civil rights activist shares legacy of love, community and action amid anecdotes of working with Dr. Martin Luther King Jr.',
+
+      ],
+      4: [
+         'Miramar pauses to acknowledge selfless service of Tuskegee Airmen',
+        'Elayne Arrington',
+        'Vance Marchbanks Jr.',
+      ]
+    }}
+    highlightText={{
+      0: "Mentions of cultural celebrations like Black History Month, Juneteenth, and MLK Day are gone.",
+      1: "So are mentions of soliders that call out their Blackness",
+      2: "Artifacts of Black culture, like soul food and gospel music, are also removed.",
+      3: "And the purge has even extended to basic facts of Black history, like slavery and civil rights activism.",
+      4: "Again, military heroes have been erased simply for being Black."
+    }}
+  />
+
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+  
+
 </main>
 
 <Footer />
